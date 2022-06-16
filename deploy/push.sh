19e2e54dba626b5f9bace08557737e6544b92daa
Egor Petenev <egor@petenev.me>
2022-06-16 18:14:19 +0300

    🧹 Skip duplicates in utility script

--- deploy/push.sh
+++ deploy/push.sh
@@ -3,5 +3,5 @@
 key=`cat ./nuget-key.secret.txt`
 
 for package in ../out/*.nupkg; do
-    dotnet nuget push $package -k $key --source https://api.nuget.org/v3/index.json
+    dotnet nuget push $package -k $key --source https://api.nuget.org/v3/index.json --skip-duplicate
 done
