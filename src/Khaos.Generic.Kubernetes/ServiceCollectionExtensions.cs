bbfd35300316dbaba9d0877dcd7af6e306bdf713
Egor Petenev <egor@petenev.me>
2025-02-09 00:55:56 +0100

    Add proper service collection helpers

--- src/Khaos.Generic.Kubernetes/ServiceCollectionExtensions.cs
+++ src/Khaos.Generic.Kubernetes/ServiceCollectionExtensions.cs
@@ -1,14 +1,18 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Khaos.Generic.Kubernetes;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddKubernetes(this IServiceCollection services, Action<Options> configure)
+    public static IServiceCollection AddKubernetesCustomResourcesClient(this IServiceCollection services, Action<Options> configureOptions)
     {
-        services.Configure(configure);
+        services.Configure(configureOptions);
         services.AddSingleton<BaseClientRegistry>();
         services.AddSingleton<ICustomResourcesClient, CustomResourcesClient>();
         return services;
     }
+
+    public static IServiceCollection AddKubernetesCustomResourcesClient(this IServiceCollection services, IConfiguration configuration)
+        => AddKubernetesCustomResourcesClient(services, configuration.Bind);
 }
